f5006dfc703061a2ecc6755f7d4980d6dd0b6c62
Mohammed Aftahi Islam <aftahiislam2016@gmail.com>
2025-02-06 16:30:22 +0600

    prsima module and service created, prisma migration completed

--- project-name/src/auth/auth.module.ts
+++ project-name/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
-import { Module } from "@nestjs/common";
-import { AuthContoller } from "./auth.controller";
-import { AuthService } from "./auth.service";
+import {
+    Module
+} from "@nestjs/common";
+import {
+    AuthContoller
+} from "./auth.controller";
+import {
+    AuthService
+} from "./auth.service";
+import {
+    PrismaModule
+} from "src/prisma/prisma.module";
 
 @Module({
+    imports: [PrismaModule],
     controllers: [AuthContoller],
     providers: [AuthService],
 })
-export class AuthModule{ }
+export class AuthModule {}
