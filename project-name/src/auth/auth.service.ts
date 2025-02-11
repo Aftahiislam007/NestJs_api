8c599b89fe7a236bcd81f1e09c9992b970da9877
Mohammed Aftahi Islam <aftahiislam2016@gmail.com>
2025-02-11 16:31:55 +0600

    docker yaml file updated and auth service file updated

--- project-name/src/auth/auth.service.ts
+++ project-name/src/auth/auth.service.ts
@@ -15,10 +15,16 @@ import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    signup(authDto: AuthDto) {
-        return {
-            msg: 'I have signed up'
-        };
+    async signup(authDto: AuthDto) {
+        const hash = await argon.hash(authDto.password);
+        const user = await this.prisma.user.create({
+            data: {
+                email: authDto.email,
+                hash,
+            },
+        });
+        
+        return user;
     }
 
     signin() {
